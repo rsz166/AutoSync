57c45935b86bca0b6c549804015258b5431adcd1
rsz166 <szabolcs.roman@yahoo.com>
2017-05-24 20:15:10 +0200

    cycle time and multibackup introduced, files now filtered by attributes

--- AutoSync/Program.cs
+++ AutoSync/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoSync
@@ -11,6 +12,10 @@ namespace AutoSync
     class Program
     {
         const string LogPath = "log.txt";
+        const string CopyPath = "/copy/";
+        const string BackupPath = "/backup/";
+        private const int MaxNumberOfBackups = 5;
+        private const string BackupFileFormat = "{0}.bak_{1:yyMMddHHmmss}";
 
         static void Main(string[] args)
         {
@@ -18,7 +23,11 @@ namespace AutoSync
             var src = args.FirstOrDefault(x => x.StartsWith("-s"))?.Substring(2);
             var dest = args.FirstOrDefault(x => x.StartsWith("-d"))?.Substring(2);
             if (args.Contains("-c")) continuous = true;
-            if(src == null || dest == null)
+            string cycleTimeString = args.FirstOrDefault(x => x.StartsWith("-t"))?.Substring(2);
+            TimeSpan cycleTime;
+            if (string.IsNullOrWhiteSpace(cycleTimeString)) cycleTime = new TimeSpan(0, 60, 0);
+            else cycleTime = new TimeSpan(0, int.Parse(cycleTimeString), 0);
+            if (src == null || dest == null)
             {
                 Console.WriteLine("No source or destination specified.");
                 Console.WriteLine("Please use -s[source] and -d[destination] options.");
@@ -26,29 +35,50 @@ namespace AutoSync
                 return;
             }
             Stopwatch stopwatch = new Stopwatch();
+            DateTime nextCheckTIme = DateTime.Now;
             do
             {
                 stopwatch.Restart();
-                SyncDirectories(src, dest);
+                SyncDirectories(src, dest + CopyPath, dest + BackupPath);
                 stopwatch.Stop();
                 Console.WriteLine("Cycle time: {0}", stopwatch.Elapsed);
+                nextCheckTIme = nextCheckTIme.Add(cycleTime);
+                while (nextCheckTIme > DateTime.Now)
+                {
+                    Thread.Sleep(1000);
+                }
             } while (continuous);
         }
 
-        private static void SyncDirectories(string src, string dest)
+        private static void SyncDirectories(string src, string dest, string backup)
         {
             foreach (var file in Directory.GetFiles(src))
             {
-                PerformSync(file, Path.Combine(dest, Path.GetFileName(file)));
+                try
+                {
+                    var fileName = Path.GetFileName(file);
+                    PerformSync(file, Path.Combine(dest, fileName), Path.Combine(backup, fileName));
+                }
+                catch(UnauthorizedAccessException)
+                {
+                    Console.WriteLine("Unauthorized file: [{0}]", src);
+                }
+                catch (Exception ex)
+                {
+                    Log("Exception caught while accessing file [{0}]: {1}", src, ex);
+                }
             }
             foreach (var dir in Directory.GetDirectories(src))
             {
-                SyncDirectories(dir, Path.Combine(dest,Path.GetDirectoryName(dir+"/")));
+                var subdir = Directory.GetParent(dir + "\\").Name;
+                SyncDirectories(dir, Path.Combine(dest, subdir), Path.Combine(backup, subdir));
             }
         }
 
-        private static void PerformSync(string src, string dest)
+        private static void PerformSync(string src, string dest, string backup)
         {
+            if ((File.GetAttributes(src) & FileAttributes.Temporary) == FileAttributes.Temporary) return;
+            if ((File.GetAttributes(src) & FileAttributes.Hidden) == FileAttributes.Hidden) return;
             if(!Directory.Exists(dest))
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(dest));
@@ -64,15 +94,35 @@ namespace AutoSync
             if (srcModTime != dstModTime)
             {
                 Log("File updated [{0}] at {1}", src, srcModTime);
+                BackupFile(dest, backup);
                 File.Copy(src, dest, true);
             }
+        }
+
+        private static void BackupFile(string dest, string backup)
+        {
+            var backupDir = Path.GetDirectoryName(backup);
+            if (!Directory.Exists(backup)) Directory.CreateDirectory(backupDir);
+            // remove older backups
+            var backupFile = Path.GetFileName(backup);
+            var backups = Directory.EnumerateFiles(backupDir).Where(x => Path.GetFileNameWithoutExtension(x) == backupFile);
+            if(backups.Count() >= MaxNumberOfBackups)
+            {
+                var backupArray = backups.ToArray();
+                Array.Sort(backupArray);
+                var oldest = backups.First();
+                File.Delete(oldest);
+            }
+            // copy new backup
+            var time = File.GetLastWriteTime(dest);
+            File.Copy(dest, string.Format(BackupFileFormat, backup, time));
         }
 
         private static void Log(string text, params object[] args)
         {
             string message = string.Format("[{0}] {1}", DateTime.Now, string.Format(text, args));
             Console.WriteLine(message);
-            using (var sw = new StreamWriter(LogPath, true))
+            using (var sw = new StreamWriter(string.Format("{0}_{1:yyyyMMdd}", LogPath, DateTime.Now), true))
             {
                 sw.WriteLine(message);
             }
